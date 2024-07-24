f2b5fad4790838897a5932b65ea8efe8d880e655
m1lmo <omerfarukkoca35@gmail.com>
2024-07-25 00:00:01 +0300

    Add tests for ImageUploadManager and PostHiveOperation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,28 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:ct_social/core/database/operation/post_hive_operation.dart';
+import 'package:ct_social/core/helper/image_upload_manager.dart';
+import 'package:ct_social/feature/model/post_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ct_social/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  late final ImageUploadManager imageUploadManager;
+  final postHiveOperation = PostHiveOperation();
+  setUp(() {
+    imageUploadManager = ImageUploadManager();
+    postHiveOperation.init();
   });
+  test('ImageUploadManager should fetch image from library', () async {
+    final result = await imageUploadManager.fetchFromLibrary();
+    expect(result, isNotNull);
+  });
+  test('post hive operation ', () async {
+    final result = postHiveOperation.box.values;
+    expect(result, List<PostModel?>);
+  });
+  // expect( , imageUploadManager.fetchFromLibrary());
 }
